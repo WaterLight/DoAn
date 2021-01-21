d11233da83c2747591ddd9df2d21437e2322dfee
waterlight <namnh620@wru.vn>
2021-01-21 18:47:57 +0700

    fix bug

--- Client-V2/src/app/views/NhanVien/NhanVienDialog.jsx
+++ Client-V2/src/app/views/NhanVien/NhanVienDialog.jsx
@@ -9,15 +9,20 @@ import {
   DialogTitle,
   DialogContent,
 } from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 // import Paper from '@material-ui/core/Paper'
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import Draggable from "react-draggable";
 import NotificationPopup from "../Component/NotificationPopup/NotificationPopup";
+import DateFnsUtils from "@date-io/date-fns";
 import {
-  addNewUrbanArea,
-  updateUrbanArea,
-  checkCode,
-} from "./NhanVienService";
+  MuiPickersUtilsProvider,
+  DateTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import { addNewUrbanArea, updateUrbanArea, checkCode } from "./NhanVienService";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -43,11 +48,28 @@ class UrbanAreaDialog extends Component {
     id: "",
     maNV: "",
     type: "",
+    name: "",
+    staffCode: "",
+    firstName: "",
+    lastName: "",
+    birthDate: new Date(),
+    birthPlaceame: "",
+    displayName: "",
+    phoneNumber: "",
+    userName: null,
+    email: "",
+    gender: "",
     description: "",
     type: "",
     shouldOpenNotificationPopup: false,
     Notification: "",
   };
+
+  listGender = [
+    { id: "M", name: "Nam" },
+    { id: "F", name: "Nữ" },
+    { id: "U", name: "Không rõ" },
+  ];
 
   handleDialogClose = () => {
     this.setState({ shouldOpenNotificationPopup: false });
@@ -72,7 +94,7 @@ class UrbanAreaDialog extends Component {
       //Nếu trả về true là code đã được sử dụng
       if (result.data) {
         toast.warning(t("general.dupli_code"));
-        
+
         // alert("Code đã được sử dụng");
       } else {
         //Nếu trả về false là code chưa sử dụng có thể dùng
@@ -101,11 +123,23 @@ class UrbanAreaDialog extends Component {
     this.setState(item);
   }
 
+  handleDateChange = (date, name) => {
+    this.setState({
+      [name]: date
+    });
+  };
+
   render() {
     let {
       id,
       maNV,
       type,
+      displayName,
+      gender,
+      birthDate,
+      phoneNumber,
+      email,
+      staffCode,
       description,
       shouldOpenNotificationPopup,
     } = this.state;
@@ -117,7 +151,6 @@ class UrbanAreaDialog extends Component {
         maxWidth="sm"
         fullWidth
       >
-       
         <DialogTitle
           style={{ cursor: "move", paddingBottom: "0px" }}
           id="draggable-dialog-title"
@@ -127,9 +160,68 @@ class UrbanAreaDialog extends Component {
 
         <ValidatorForm ref="form" onSubmit={this.handleFormSubmit}>
           <DialogContent>
-            <Grid className="" container spacing={2}>
-
-              <Grid item sm={12} xs={12}>
+            <Grid className=" w-100" container spacing={2}>
+              <Grid item sm={5} xs={12}>
+                <TextValidator
+                  className=" w-100"
+                  label={
+                    <span>
+                      <span style={{ color: "red" }}>*</span>
+                      {t("staff.displayName")}
+                    </span>
+                  }
+                  onChange={this.handleChange}
+                  type="text"
+                  name="displayName"
+                  value={displayName}
+                  validators={["required"]}
+                  errorMessages={[t("general.required")]}
+                />
+              </Grid>
+              <Grid item sm={3} xs={12}>
+                <FormControl fullWidth={true}>
+                  <InputLabel htmlFor="gender-simple">
+                    {t("staff.gender")}
+                  </InputLabel>
+                  <Select
+                    value={gender}
+                    onChange={(gender) => this.handleChange(gender, "gender")}
+                    inputProps={{
+                      name: "gender",
+                      id: "gender-simple",
+                    }}
+                  >
+                    {this.listGender.map((item) => {
+                      return (
+                        <MenuItem key={item.id} value={item.id}>
+                          {item.name}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    margin="none"
+                    fullWidth
+                    id="date-picker-dialog mt-2"
+                    // style={{marginTop:'2px'}}
+                    label={t("staff.birthDate")}
+                    format="dd/MM/yyyy"
+                    value={birthDate}
+                    onChange={(date) =>
+                      this.handleDateChange(date, "birthDate")
+                    }
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                    invalidDateMessage={t("general.invalidDateFormat")}
+                  />
+                </MuiPickersUtilsProvider>
+              </Grid>
+              <Grid item sm={3} xs={12}>
                 <TextValidator
                   className="w-100 "
                   label={
@@ -146,17 +238,54 @@ class UrbanAreaDialog extends Component {
                   errorMessages={[t("general.required")]}
                 />
               </Grid>
-              <Grid item sm={12} xs={12}>
+              <Grid item sm={3} xs={12}>
+                <TextValidator
+                  className=" w-100"
+                  label={
+                    <span>
+                      <span style={{ color: "red" }}>*</span>
+                      {t("staff.phoneNumber")}
+                    </span>
+                  }
+                  onChange={this.handleChange}
+                  type="text"
+                  name="phoneNumber"
+                  value={phoneNumber}
+                  validators={["required", "minNumber:0"]}
+                  errorMessages={[
+                    t("general.required"),
+                    t("general.invalidFormat"),
+                  ]}
+                />
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <TextValidator
+                  className=" w-100"
+                  label={
+                    <span>
+                      <span style={{ color: "red" }}>*</span>
+                      {t("staff.email")}
+                    </span>
+                  }
+                  onChange={this.handleChange}
+                  type="text"
+                  name="email"
+                  value={email}
+                  validators={["required", "isEmail"]}
+                  errorMessages={[t("general.required"), t("general.invalidFormat"),]}
+                />
+              </Grid>
+              <Grid item sm={2} xs={12}>
                 <TextValidator
                   className="w-100 "
                   label={
                     <span>
                       <span style={{ color: "red" }}>*</span>
-                      {t("general.typeStaff")}
+                      {t("Loại")}
                     </span>
                   }
                   onChange={this.handleChange}
-                  type="text"
+                  type="number"
                   name="type"
                   value={type}
                   validators={["required"]}
