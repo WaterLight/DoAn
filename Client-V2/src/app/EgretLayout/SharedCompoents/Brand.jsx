a774b4c7eae2e71db50a913055b48bf479d15b31
waterlight <namnh620@wru.vn>
2021-02-02 09:33:03 +0700

    dfdf

--- Client-V2/src/app/EgretLayout/SharedCompoents/Brand.jsx
+++ Client-V2/src/app/EgretLayout/SharedCompoents/Brand.jsx
@@ -15,7 +15,7 @@ class Brand extends Component {
         </div> */}
         <div className="flex flex-middle brand logo-menu">
           <Link to={ConstantList.HOME_PAGE}>
-            <span className="brand__text">SHOE SHOP</span>
+            <span className="brand__text">SHOE SNEAKERS</span>
           </Link>
         </div>
         {this.props.children}
